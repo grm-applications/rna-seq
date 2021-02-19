2ee624ca998aa0295858addbbdf14099f39e71ea
George Moore <georgemoore2899@gmail.com>
2021-02-19 14:07:40 +0000

    altered the fasq-dump script

--- modules/sra_fastq_dump.sh
+++ modules/sra_fastq_dump.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
 
-fastq-dump () {
-	cd ..
-	cd FASTQ
-	for dir in *
-	do
-		cd $dir
-		for file in *
-		do
-			if [[ $file == *".sra"* ]]
-			then
-				../../modules/sratoolkit.2.10.9-mac64/bin/fasterq-dump $file
-			fi
-		done
-		cd ..
-	done
-}
 
-fastq-dump-split () {
-	cd ..
-	cd FASTQ
-	for dir in *
+cd ..
+cd FASTQ
+for dir in *
+do
+	cd $dir
+	for file in *
 	do
-		cd $dir
-		for file in *
-		do
-			if [[ $file == *".sra"* ]]
-			then
-				../../modules/sratoolkit.2.10.9-mac64/bin/fasterq-dump --split-files $file
-			fi
-		done
-		cd ..
+		if [[ $file == *".sra"* ]]
+		then
+			../../modules/sratoolkit.2.10.9-mac64/bin/fasterq-dump --split-files $file
+		fi
 	done
-}
-
-echo "Split files?"
-select yn in "Yes" "No"; do
-case $yn in
-	Yes ) fastq-dump-split; break;;
-	No ) fastq-dump; break;;
-esac
+	cd ..
 done
+
 
 
