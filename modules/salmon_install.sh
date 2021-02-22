dce200acad003dae2c90f7d35a53d799c1fb6256
George Moore <georgemoore2899@gmail.com>
2021-02-22 12:04:44 +0000

    improved Salmon compatibility with Linux

--- modules/salmon_install.sh
+++ modules/salmon_install.sh
@@ -1,3 +1,14 @@
-wget https://github.com/COMBINE-lab/salmon/releases/download/v0.99.0-beta2/salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
-tar xzvf salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
-rm salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
+
+# if [ `uname` == "Darwin" ]; then
+# 	echo "Operating System is macOS"
+# 	wget https://github.com/COMBINE-lab/salmon/releases/download/v0.99.0-beta2/salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
+# 	tar xzvf salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
+# 	rm salmon_0.99.0_beta2_OSX_10.14.5.tar.gz
+# elif [ `uname` == "Linux" ]; then
+	echo "Operting System is Linux"
+	wget https://github.com/COMBINE-lab/salmon/releases/download/v1.4.0/salmon-1.4.0_linux_x86_64.tar.gz
+	tar xzvf salmon-1.4.0_linux_x86_64.tar.gz
+	rm salmon-1.4.0_linux_x86_64.tar.gz
+# fi
+
+
--- modules/sra_install.sh
+++ modules/sra_install.sh
@@ -1,12 +1,12 @@
 
-# if [ `uname` == "Darwin" ]; then
-# 	echo "Operating System is macOS"
-# 	wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.tar.gz
-# 	tar -xzvf sratoolkit.current-mac64.tar.gz
-# 	rm sratoolkit.current-mac64.tar.gz
-# elif [ `uname` == "Linux" ]; then
+if [ `uname` == "Darwin" ]; then
+	echo "Operating System is macOS"
+	wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.tar.gz
+	tar -xzvf sratoolkit.current-mac64.tar.gz
+	rm sratoolkit.current-mac64.tar.gz
+elif [ `uname` == "Linux" ]; then
 	echo "Operting System is Linux"
 	wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz
 	tar -xzvf sratoolkit.current-centos_linux64.tar.gz
 	rm sratoolkit.current-centos_linux64.tar.gz
-# fi
+fi
