4909928520125fd5381d816cedc2c29e8fdaad2b
George Moore <georgemoore2899@gmail.com>
2021-02-23 16:46:27 +0000

    small change

--- modules/salmon_run.sh
+++ modules/salmon_run.sh
@@ -1,6 +1,5 @@
 
 
-
 if [ `uname` == "Darwin" ]; then
 	cd ..
 	cd FASTQ
@@ -8,7 +7,7 @@ if [ `uname` == "Darwin" ]; then
 	do
 		echo "Processing sample ${dir}"
 		cd ${dir}
-			../../modules/salmon_0.99.0_beta2_OSX_10.14.5/bin/salmon quant -i ../../Reference/athal_index -l A -1 ${dir}_1.fastq -2 ${dir}_2.fastq -p 8 --validateMappings -o ../../Salmon/${dir}
+			../../modules/salmon_0.99.0_beta2_OSX_10.14.5/bin/salmon quant -i ../../Reference/ -l A -1 ${dir}_1.fastq -2 ${dir}_2.fastq -p 8 --validateMappings -o ../../Salmon/${dir}
 		cd ..
 
 	done
@@ -19,7 +18,7 @@ elif [ `uname` == "Linux" ]; then
 	do
 		echo "Processing sample ${dir}"
 		cd ${dir}
-			../../modules/salmon_0.99.0_beta2_linux_x86_64/bin/salmon quant -i ../../Reference/athal_index -l A -1 ${dir}_1.fastq -2 ${dir}_2.fastq -p 8 --validateMappings -o ../../Salmon/${dir}
+			../../modules/salmon_0.99.0_beta2_linux_x86_64/bin/salmon quant -i ../../Reference/ -l A -1 ${dir}_1.fastq -2 ${dir}_2.fastq -p 8 --validateMappings -o ../../Salmon/${dir}
 		cd ..
 
 	done
