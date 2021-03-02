98d4198859c6ddd872d03e7268e8b2722be18b8e
George Moore <georgemoore2899@gmail.com>
2021-03-02 11:34:48 +0000

    changed workflow

--- workflow.sh
+++ workflow.sh
@@ -12,7 +12,7 @@ if [ `uname` == "Linux" ];then
 	# module load igmm/apps/HISAT2/2.1.0
 	# module load igmm/apps/salmon/1.3.0
 	# module load igmm/apps/sratoolkit/2.10.8
-	# module load igmm/apps/Trinity/2.8.3
+	module load igmm/apps/Trinity/2.8.3
 elif [ `uname` == "Darwin" ];then
 	cd modules
 	cd SRA && ./sra_install.sh && cd ..
@@ -25,15 +25,15 @@ fi
 
 # ./modules/SRA/sra_prefetch.sh
 # ./modules/SRA/sra_fastq_dump.sh
-./modules/FastQC/fastqc_run.sh
+# ./modules/FastQC/fastqc_run.sh
 # ./modules/Salmon/salmon_download_reference.sh
 # ./modules/Salmon/salmon_index_reference.sh
 # ./modules/Salmon/salmon_run.sh
 
-# python ./modules/Trinity/trinity_sample_file.py
-# ./modules/Trinity/trinity_rearrange_files.sh
-
+python ./modules/Trinity/trinity_sample_file.py
+./modules/Trinity/trinity_rearrange_files.sh
 # ./modules/Trinity/trinity_run.sh
+
 # Rscript ./modules/DESeq2/deseq2.R
 # ./hisat2_download_reference.sh
 # ./hisat2_index_reference.sh
