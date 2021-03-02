eef18bae90637b1d93ae34b542e80b23753a5b9a
George Moore <georgemoore2899@gmail.com>
2021-03-02 18:22:09 +0000

    changed memory allocation

--- modules/Trinity/trinity_run.sh
+++ modules/Trinity/trinity_run.sh
@@ -3,6 +3,6 @@ cd FASTQ
 if [ `uname` == "Darwin" ]; then
 	../modules/Trinity/trinityrnaseq-v2.11.0/Trinity --seqType fq --samples_file "trinity_sample_file.txt" --CPU 6 --max_memory 20G
 elif [ `uname` == "Linux" ]; then
-	Trinity --seqType fq --samples_file "trinity_sample_file.txt" --CPU 6 --max_memory 200G
+	Trinity --seqType fq --samples_file "trinity_sample_file.txt" --CPU 16 --max_memory 50G
 fi
 cd ..
