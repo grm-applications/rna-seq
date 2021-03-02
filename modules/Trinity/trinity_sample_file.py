219414c2933780b7dd7f6c5db886b25e82d11fb8
George Moore <georgemoore2899@gmail.com>
2021-03-02 14:23:29 +0000

    did work on Trinity

--- modules/Trinity/trinity_sample_file.py
+++ modules/Trinity/trinity_sample_file.py
@@ -4,15 +4,30 @@
 import os
 import csv
 
+def fastq_filter(str):
+	if "fastq" in str:
+		return True
+	else:
+		return False
+
 dir = "./FASTQ/"
-accessions=[]
+accessions={}
 
 for item in os.listdir("./FASTQ"):
-	path = dir + item
-	if os.path.isdir(path):
-		accessions.append(item)
+	if item == ".DS_Store":
+		continue
+	filtered = filter(fastq_filter, os.listdir(dir+item))
+
+	if len(filtered) == 2:
+		accessions[item] = "paired"
+	elif len(filtered) == 1:
+		accessions[item] = "single"
 
 with open("./FASTQ/trinity_sample_file.txt", "w") as file:
-	for number in accessions:
-		file.write(number + "\t" + number + "_rep1" + "\t" + number + "_1.fq" + "\t" + number + "_2.fq")
-		file.write("\n")
+	for accession in accessions:
+		if accessions[accession] == "paired":
+			file.write(accession + "\t" + accession + "_rep1" + "\t" + accession + "_1.fq" + "\t" + accession + "_2.fq")
+			file.write("\n")
+		elif accessions[accession] == "single":
+			file.write(accession + "\t" + accession + "_rep1" + "\t" + accession + "_1.fq")
+			file.write("\n")
--- workflow.sh
+++ workflow.sh
@@ -8,10 +8,10 @@ if [ `uname` == "Linux" ];then
 	. /etc/profile.d/modules.sh
 	module load python/3.4.3
 	module load igmm/apps/R/4.0.3
-	module load igmm/apps/FastQC/0.11.9
+	# module load igmm/apps/FastQC/0.11.9
 	# module load igmm/apps/HISAT2/2.1.0
 	# module load igmm/apps/salmon/1.3.0
-	# module load igmm/apps/sratoolkit/2.10.8
+	module load igmm/apps/sratoolkit/2.10.8
 	module load igmm/apps/Trinity/2.8.3
 elif [ `uname` == "Darwin" ];then
 	cd modules
@@ -23,8 +23,8 @@ elif [ `uname` == "Darwin" ];then
 	cd ..
 fi
 
-# ./modules/SRA/sra_prefetch.sh
-# ./modules/SRA/sra_fastq_dump.sh
+./modules/SRA/sra_prefetch.sh
+./modules/SRA/sra_fastq_dump.sh
 # ./modules/FastQC/fastqc_run.sh
 # ./modules/Salmon/salmon_download_reference.sh
 # ./modules/Salmon/salmon_index_reference.sh
