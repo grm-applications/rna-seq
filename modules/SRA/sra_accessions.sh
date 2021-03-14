ac8a2f9f1cf57b7adad0b01eade4add3e3fa9db7
George Moore <georgemoore2899@gmail.com>
2021-03-14 12:32:00 +0000

    changed accession downloading

--- modules/SRA/sra_accessions.sh
+++ modules/SRA/sra_accessions.sh
@@ -5,7 +5,7 @@ prefetch --option-file ./config/single_accessions.txt -O ./FASTQ/
 cd FASTQ
 for dir in */;do
 	cd $dir
-		fastq-dump --defline-seq '@$sn[_$rn]/$ri' ./$dir -O ./
+		fastq-dump --defline-seq '@$sn[_$rn]/$ri' $dir -O ./
 		rm -rf ${dir}.sra
 	cd ..
 done
@@ -15,7 +15,7 @@ prefetch --option-file ./config/paired_accessions.txt -O ./FASTQ/
 cd FASTQ
 for dir in */;do
 	cd $dir
-		fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files ./$dir -O ./
+		fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files $dir -O ./
 		rm -rf ${dir}.sra
 	cd ..
 done
