c130349a1d75fd22f6a46c07b7786e9aa4959b10
George Moore <georgemoore2899@gmail.com>
2021-03-15 21:59:41 +0000

    changed workflow

--- workflow.sh
+++ workflow.sh
@@ -6,21 +6,12 @@
 
 . /etc/profile.d/modules.sh
 
-# ./modules/SRA/sra_accessions.sh
+./modules/SRA/sra_accessions.sh
 # ./modules/FastQC/fastqc_run.sh
-# ./modules/MultiQC/multiqc_install.sh
-./modules/MultiQC/multiqc_run.sh
+# ./modules/MultiQC/multiqc_run.sh
 # ./modules/Trinity/trinity_run.sh
-
-
-# ./modules/Salmon/salmon_download_reference.sh
+./modules/Salmon/salmon_download_reference.sh
 # ./modules/BLAST/blast_run.sh
-# ./modules/Salmon/salmon_index_reference.sh
-# ./modules/Salmon/salmon_run.sh
-# ./modules/DESeq2/deseq2_run.sh
-
-
-
-# ./hisat2_download_reference.sh
-# ./hisat2_index_reference.sh
-# ./hisat2_run.sh
+./modules/Salmon/salmon_index_reference.sh
+./modules/Salmon/salmon_run.sh
+./modules/DESeq2/deseq2_run.sh
