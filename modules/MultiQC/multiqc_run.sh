1b751951ceb4bd6874f354c5c1e47f6858dd41dd
George Moore <georgemoore2899@gmail.com>
2021-03-15 21:45:43 +0000

    made changes

--- modules/MultiQC/multiqc_run.sh
+++ modules/MultiQC/multiqc_run.sh
@@ -6,4 +6,10 @@ cd MultiQC
 pip install .
 cd ..
 python ./modules/Python/multiqc_run.py
+
+rm -rf MultiQC
+mkdir MultiQC
+mv multiqc_data ./MultiQC/multiqc_data
+mv multiqc_report.html ./MultiQC/multiqc_report.html
+
 module unload roslin/python/3.8.1
